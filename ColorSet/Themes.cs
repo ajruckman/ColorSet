6a35b8f1f8628f4946b0035d879d6aedab67336a
A.J. Ruckman <aj@ruckman.dev>
2020-02-23 11:14:18 -0500

    ColorSet 1.0.0-alpha: Initial commit

---
+++ ColorSet/Themes.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+namespace ColorSet
+{
+    internal static class Specs
+    {
+        internal static readonly List<string> Themes = new List<string>
+        {
+            "Light",
+            "Dark",
+            "Pink",
+            "Matrix"
+        };
+    }
+}
